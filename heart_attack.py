ac627722d3817cbde3a0e683ce8e39d25588a401
MilkTheBev <jokethrow@gmail.com>
2025-07-06 03:23:48 +0530

    .

--- heart_attack.py
+++ heart_attack.py
@@ -125,7 +125,9 @@ try:
         lins.append(gap + lin + gap2)
         if mode == 0:
             if not ttymode:
-                time.sleep(0.0625)                                                      # Waits 1/16th of a second
+                time.sleep(0.0625)                                                      # Waits 1/16th of a second if its a tty, if it isnt it just slows down by a lot. I underestimate non tty engironments.
+            else:
+                time.sleep(0.25)
             if num == cols:
                 opr = " - "                                                         # If the number of foreground characters is the same as the number of columns, operation becomes subtraction
             elif num == num2:
